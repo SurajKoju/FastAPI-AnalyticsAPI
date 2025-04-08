061126cf196d660b88f72158b9952daf6838c0d9
surajkoju <surajkoju99@gmail.com>
2025-04-08 13:58:30 +0545

    POST Request

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,18 +1,29 @@
 from fastapi import APIRouter
-from .schemas import EventSchema
+from .schemas import EventSchema, EventListSchema
 
 
 router = APIRouter()
 
 
 @router.get("/")
-def read_events():
+def read_events() -> EventListSchema:
     return {
-        "results": [1,2,3]
+        "results": [{"id": 1}, {"id": 2}, {"id": 3}],
+        "count": 3
     }
     
-@router.get("/{event_id}")
 
+'''
+data: dict This parameter takes the user-sent data and returns the same data. 
+            The variable type is important for returning the data correctly.
+'''    
+
+@router.post("/")
+def create_event(data:dict):
+    return data
+    
+    
+@router.get("/{event_id}")
 # This parts represents the data to be sent to schema
 def get_events(event_id:int) -> EventSchema:
     return {
