b80d4f70be281e0f44d8833ab9f74fa85af3bdb6
surajkoju <surajkoju99@gmail.com>
2025-04-10 13:14:08 +0545

    Complete SQL Model

--- src/api/events/models.py
+++ src/api/events/models.py
@@ -1,10 +1,27 @@
 from typing import List, Optional
 from sqlmodel import SQLModel, Field
+from datetime import datetime, timezone
+import sqlmodel
+
+def get_utc_now():
+    return datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
 
 class EventModel(SQLModel, table=True):
     id: Optional[int] = Field(default = None, primary_key=True)
     page: Optional[str] = ""
     description: Optional[str] = ""
+    created_at: datetime = Field(
+        default_factory = get_utc_now,
+        sa_type = sqlmodel.DateTime(timezone=True),
+        nullable = False
+    )
+    
+    updated_at: datetime = Field(
+        default_factory = get_utc_now,
+        sa_type = sqlmodel.DateTime(timezone=True),
+        nullable = False
+    )
+    
           
 class EventCreateSchema(SQLModel):
     page: str
